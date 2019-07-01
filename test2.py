a0cb094a38fa197514ecf65b00cbe6beaacc3497
xidianzyf <1143942196@qq.com>
2019-07-01 20:11:53 +0800

    update

--- test2.py
+++ test2.py
@@ -3,9 +3,8 @@ import torch
 class_num = 10
 batch_size = 4
 label = torch.LongTensor(batch_size, 1).random_() % class_num
-print(label)
+label = label.cuda()
 print(label.size())
 
 one_hot = torch.zeros(batch_size, class_num).scatter_(1, label, 1)
 print(one_hot)
-print(one_hot.size())
