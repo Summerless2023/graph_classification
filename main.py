fb1e70b51144fa09da3d27676c8008df51b4c25b
xidianzyf <1143942196@qq.com>
2019-07-10 18:59:50 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import torch
+import numpy as np
 from torch import nn, optim
 from load_data import init_data
-from load_data import load_to_dataset
 from my_model import GraphClassifier
 from utils import handle_graph
 
@@ -15,7 +15,7 @@ learning_rate = 0.01
 num_epoches = 1
 class_num = 2
 index = int(1178 * 0.7)
-
+# index = 3
 if __name__ == '__main__':
 	my_graphs, max_node_num1, max_node_num2 = init_data(datadir, dataname)
 	print("数据处理完成")
@@ -33,20 +33,27 @@ if __name__ == '__main__':
 			input1, adj1 = handle_graph(my_graphs[i].graph1, max_node_num1)
 			input2, adj2 = handle_graph(my_graphs[i].graph2, max_node_num2)
 			label = my_graphs[i].label
-			input1 = torch.from_numpy(input1).cuda()
-			print('input1.size = ', input1.size())
-			input2 = torch.from_numpy(input2).cuda()
-			print('input2.size = ', input2.size())
+			input1 = torch.from_numpy(input1).float().cuda()
+			# print('input1.size = ', input1.size())
+			input2 = torch.from_numpy(input2).float().cuda()
+			# print('input2.size = ', input2.size())
+			adj1 = np.pad(adj1, ((0, max_node_num1 - len(adj1[0])), (0, max_node_num1 - len(adj1[0]))),
+			              'constant', constant_values=((0, 0), (0, 0)))
 			adj1 = torch.from_numpy(adj1).cuda()
-			print('adj1.size = ', adj1.size())
+			# print('adj1.size = ', adj1.size())
+			adj2 = np.pad(adj2, ((0, max_node_num2 - len(adj2[0])), (0, max_node_num2 - len(adj2[0]))),
+			              'constant', constant_values=((0, 0), (0, 0)))
 			adj2 = torch.from_numpy(adj2).cuda()
-			print('adj2.size = ', adj2.size())
-			print('label = ', label)
+			# print('adj2.size = ', adj2.size())
+
+			tmp = np.zeros((1, 1), dtype=np.int)
+			tmp[0][0] = label
+			tmp_label = torch.from_numpy(tmp)
 			output = model.forward(input1, input2, adj1, adj2).cuda()
-			cu_label = torch.zeros(batch_size, class_num).scatter_(1, label, 1).cuda()
-			loss = criterion(output, cu_label)
+			one_hot = torch.zeros(batch_size, class_num).scatter_(1, tmp_label, 1).cuda()
+			loss = criterion(output, one_hot)
 			print_loss = loss.data.item()
-			print("loss : ", print_loss)
+			print(print_loss)
 			if i % 5 == 0:
 				optimizer.zero_grad()
 				loss.backward()
@@ -61,20 +68,33 @@ if __name__ == '__main__':
 			input1, adj1 = handle_graph(my_graphs[i].graph1, max_node_num1)
 			input2, adj2 = handle_graph(my_graphs[i].graph2, max_node_num2)
 			label = my_graphs[i].label
-			input1 = torch.from_numpy(input1).cuda()
-			print('input1.size = ', input1.size())
-			input2 = torch.from_numpy(input2).cuda()
-			print('input2.size = ', input2.size())
+			input1 = torch.from_numpy(input1).float().cuda()
+			# print('input1.size = ', input1.size())
+			input2 = torch.from_numpy(input2).float().cuda()
+			# print('input2.size = ', input2.size())
+
+			adj1 = np.pad(adj1, ((0, max_node_num1 - len(adj1[0])), (0, max_node_num1 - len(adj1[0]))),
+			              'constant', constant_values=((0, 0), (0, 0)))
 			adj1 = torch.from_numpy(adj1).cuda()
-			print('adj1.size = ', adj1.size())
+			# print('adj1.size = ', adj1.size())
+			adj2 = np.pad(adj2, ((0, max_node_num2 - len(adj2[0])), (0, max_node_num2 - len(adj2[0]))),
+			              'constant', constant_values=((0, 0), (0, 0)))
 			adj2 = torch.from_numpy(adj2).cuda()
-			print('adj2.size = ', adj2.size())
-			print('label.size = ', label.size())
-			print('label = ', label)
+			#print('adj2.size = ', adj2.size())
+
 			output = model.forward(input1, input2, adj1, adj2).cuda()
 			_, pre = torch.max(output, dim=1)
+			tmp = np.zeros((1, 1), dtype=np.int)
+			tmp[0][0] = label
+			tmp_label = torch.from_numpy(tmp)
 			pre = pre.cuda()
-			label = label.cuda()
-			if pre[0] == label[0]:
+			tmp_label = tmp_label.cuda()
+			print("pre")
+			print(pre.size())
+			print(pre)
+			print("label")
+			print(tmp_label.size())
+			print(tmp_label)
+			if pre[0] == tmp_label[0][0]:
 				acc_num += 1
 		print("accuracy : ", acc_num, "of ", test_count)
